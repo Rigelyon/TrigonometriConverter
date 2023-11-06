01da78a6dc3f5187bbe9c5c47525813b39f277ff
Rigelyon <pandudwi120@gmail.com>
2023-11-06 18:47:14 +0700

    adding changes to front page

--- ProgramFungsi/gui.py
+++ ProgramFungsi/gui.py
@@ -5,66 +5,79 @@ size = (800, 600)
 
 app = ctk.CTk()
 app.title("Program Fungsi")
+# app.iconbitmap("ProgramFungsi\icon.png")
 app.geometry(f"{size[0]}x{size[1]}")
 app.minsize(size[0], size[1])
+app.grid_columnconfigure(0, weight=1)
+app.grid_rowconfigure(0, weight=1)
 
 main_frame = ctk.CTkFrame(master=app, fg_color="white", corner_radius=None)
 main_frame.pack(expand=True, fill="both")
 
+def page_raise(page):
+    pass
+
+def about_page():
+    frame_size = (800,600)
+    frame = ctk.CTkFrame(
+        master=main_frame,
+        width=frame_size[0],
+        height=frame_size[1],
+        corner_radius=None,
+        border_width=3,
+        border_color="#1e88e5",
+        fg_color="white",       
+    )
+    frame.pack()
+    frame.lift()
+
 
 def front_page(parent):
-    # half_color = ctk.CTkFrame(
-    #     master=parent,
-    #     height=size[1] / 2,
-    #     fg_color="#2196f3",
-    # )
-    # half_color.pack(expand=True, fill="x", side="top", anchor="n")
+    main_frame.configure(fg_color="#2196f3")
+    frame_size = (350,350)
 
     frame = ctk.CTkFrame(
         master=parent,
-        width=400,
-        height=300,
+        width=frame_size[0],
+        height=frame_size[1],
         corner_radius=None,
-        border_width=None,
-        border_color="#0d47a1",
-        fg_color="#2196f3",
+        border_width=3,
+        border_color="#1e88e5",
+        fg_color="white",
     )
-    frame.place(x=size[0] / 2, y=(size[1] / 2), anchor="center")
+    frame.place(relx=0.5, rely=0.5, anchor="center")
 
     title = ctk.CTkLabel(
-        master=parent,
-        width=400,
-        height=50,
+        master=frame,
         text="Program Fungsi",
-        text_color="black",
+        text_color="#2196f3",
         font=ctk.CTkFont(family="Helvetica", size=36, weight="bold"),
     )
-    title.place(x=size[0] / 2, y=(size[1] / 2) - 200, anchor="center")
+    title.place(x=frame_size[0]/2,y=45, anchor="center")
 
+    mulai_button = ctk.CTkButton(
+        master=frame,
+        width=200,
+        height=45,
+        fg_color="#64b5f6",
+        text="Mulai",
+        text_color="white",
+        font=ctk.CTkFont(family="Helvetica", size=18, weight="normal"),
+    )
+    mulai_button.place(x=frame_size[0]/2, y=(frame_size[1])-75, anchor="s")
 
-front_page(main_frame)
-
-# class App(ctk.CTk):
-#     def __init__(self, title, size):
-#         super().__init__()
-#         self.title(title)
-#         self.geometry(f"{size[0]},{size[1]}")
-#         self.minsize(size[0], size[1])
-
-#         self.frame = MainFrame(self).pack(expand=True, fill="both")
-
-
-# class MainFrame(ctk.CTkFrame):
-#     def __init__(self, master, **kwargs):
-#         super().__init__(master, **kwargs)
-#         # ctk.CTkLabel(self, anchor="n", fg_color="red").pack(expand=True, fill="both")
-#         self.front_page(master=self).pack(expand=True, fill="both")
-
-#     def front_page(self, master):
-#         frame = ctk.CTkFrame(self).pack()
-#         ctk.CTkLabel(master= frame, anchor="n", fg_color="blue").pack(expand=True, fill="both")
-#         return frame
-
+    about_button = ctk.CTkButton(
+        master=frame,
+        width=200,
+        height=45,
+        fg_color="#64b5f6",
+        text="About",
+        text_color="white",
+        font=ctk.CTkFont(family="Helvetica", size=18, weight="normal"),
+        command=about_page
+    )
+    about_button.place(x=frame_size[0]/2, y=(frame_size[1])-20, anchor="s")
 
 if __name__ == "__main__":
+    front_page(main_frame)
     app.mainloop()
